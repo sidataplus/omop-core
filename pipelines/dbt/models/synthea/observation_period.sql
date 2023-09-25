e20cb05e3c067bb3b55e571c4b8433540d661a67
Thanapat Pitchayarat <thane_pi@outlook.com>
2023-09-26 05:23:43 +0700

    fix: Synthea Pipeline

--- pipelines/dbt/models/synthea/observation_period.sql
+++ pipelines/dbt/models/synthea/observation_period.sql
@@ -1,16 +1,16 @@
-
-
-select row_number()over(order by start_date, person_id) AS observation_period_id,
-       person_id AS person_id,
-	   start_date AS observation_period_start_date,
-	   end_date AS observation_period_end_date,
-	   44814724 AS period_type_concept_id -- 44814724::https://athena.ohdsi.org/search-terms/terms/44814724
-  from (
-select p.person_id,
-       min(e.start) start_date,
-	   max(e.stop) end_date
-  FROM {{ ref('person') }} p
-  JOIN {{ source('synthea','encounters') }} e
-    on p.person_source_value = e.patient
- group by p.person_id
-       ) tmp
+SELECT
+    ROW_NUMBER()OVER(ORDER BY person_id) AS observation_period_id,
+    person_id AS person_id,
+    start_date AS observation_period_start_date,
+    end_date AS observation_period_end_date,
+    44814724 AS period_type_concept_id
+FROM (
+    SELECT
+        p.person_id,
+        MIN(e.start) AS start_date,
+        MAX(e.stop) AS end_date
+    FROM {{ ref('person') }} AS p
+    INNER JOIN {{ source('synthea', 'encounters') }} AS e
+        ON p.person_source_value = e.patient
+    GROUP BY p.person_id
+) AS tmp
